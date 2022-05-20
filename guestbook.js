5621e2c720849c039a0db25ba3579982c207e80f
misterpooh <33590996+misterpooh@users.noreply.github.com>
2022-05-20 05:19:06 -0400

    inserted api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7609aef9.us-east.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
